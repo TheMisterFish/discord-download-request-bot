c2dc9decd76b05117e482513830850587fe07106
Fish <vincentvenhuizen1997@gmail.com>
2024-09-24 19:31:15 +0200

    small help command footer update

--- cogs/helpCommand.py
+++ cogs/helpCommand.py
@@ -47,6 +47,7 @@ class HelpCommand(commands.Cog):
             embed.add_field(name="/config search_regex [regex]", value="Configure the search regex for download messages.", inline=False)
             embed.add_field(name="/config reset_regex", value="Reset the search regex to default (DN : (.+)).", inline=False)
 
+        embed.add_field(name="\u200b", value="\u200b", inline=False)
         embed.set_footer(text="Made by Mister Fish with special thanks to Orange dot.")
 
         await ctx.respond(embed=embed, ephemeral=True)
